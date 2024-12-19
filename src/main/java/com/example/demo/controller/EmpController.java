2797f0df56554d5522a28e20a8b7a47e56a2b115
Thejaswini E C <136478774+Thejpc@users.noreply.github.com>
2024-12-19 10:32:06 +0530

    Update EmpController.java

--- src/main/java/com/example/demo/controller/EmpController.java
+++ src/main/java/com/example/demo/controller/EmpController.java
@@ -9,12 +9,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Employee;
 import com.example.demo.service.EmployeeService;
 
+@CrossOrigin("*")
 @RestController
 public class EmpController {
     @Autowired
